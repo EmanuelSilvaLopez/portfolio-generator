e00d4ba59bc6a3d37f3e16cb2c34471c19ab0285
EmanSL <emanuelsilvalopez@gmail.com>
2022-01-31 14:26:11 -0500

    staging changes

--- app.js
+++ app.js
@@ -1,16 +1,6 @@
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 const inquirer = require('inquirer');
-const fs = require('fs');
 const generatePage = require('./src/page-template');
-
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile('./index.html', pageHTML, err => {
-//   if (err) throw err;
-
-//   console.log('Portfolio complete! Check out index.html to see the output!');
-// });
-
-
 
 const promptUser = () => {
   return inquirer.prompt([
@@ -142,10 +132,18 @@ Add a New Project
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    const pageHTML = generatePage(portfolioData);
-
-    fs.writeFile('./index.html', pageHTML, err => {
-      if (err) throw new Error(err);
-
-    });
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
   });
