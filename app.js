f2fca5d62f98d9728aec78e05e6574af70c0b27a
EmanSL <emanuelsilvalopez@gmail.com>
2022-01-31 14:28:35 -0500

    working changes

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const { writeFile, copyFile } = require('./utils/generate-site.js');
+const { writeFile, copyFile } = require('./utils/generate-site');
 const inquirer = require('inquirer');
 const generatePage = require('./src/page-template');
 
